514cb1bdb019f40bddd8927d4d58e6dc12c552ad
rjk-info <therjkinfo@gmail.com>
2025-02-26 22:30:58 +0530

    c

---
+++ MyCode/Pattern/code/_1_StarP.java
@@ -0,0 +1,10 @@
+public class _1_StarP {
+    public static void main(String[] args) {
+        for(int i = 1 ; i<=4 ; i++) {
+            for(int j = 1 ; j<=i ; j++) {
+                System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
