4e03caec0404c42b1f892bc3ddf779e0606f28ab
rjk-info <therjkinfo@gmail.com>
2025-03-31 19:19:09 +0530

    add code

--- MyCode/Pattern/code/_1_StarP.java
+++ MyCode/Pattern/code/_1_StarP.java
@@ -3,6 +3,8 @@ public class _1_StarP {
         for(int i = 1 ; i<=4 ; i++) {
             for(int j = 1 ; j<=i ; j++) {
                 System.out.print("*");
+                System.out.print("*");
+                System.out.print("*");
         }
         System.out.println();
     }
