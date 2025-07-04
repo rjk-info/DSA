a6134496afb6436b9420b0cde4e80d8b51f97ebf
rjk-info <therjkinfo@gmail.com>
2025-07-04 17:39:14 +0530

    update pq3

--- MyCode/2D Array/PQ3.java
+++ MyCode/2D Array/PQ3.java
@@ -1,3 +1,24 @@
-public class PQ3 {
-    
+//leetcode problem 867. Transpose Matrix
+class Solution {
+    public int[][] transpose(int[][] arr) {
+         int rows = arr.length;
+        int cols = arr[0].length;
+        int [][] tran = new int[cols][rows] ;
+
+        
+         for(int i = 0 ; i<rows;i++){
+            for(int j =0 ;j<cols;j++){
+              tran[j][i] = arr[i][j];
+            }
+         }
+        
+         for(int i = 0 ; i<cols;i++){
+            for(int j =0 ;j<rows;j++){
+              System.out.print(tran[i][j]+" ");
+            }
+            System.out.println();
+            
+    }
+    return tran;
+}
 }
