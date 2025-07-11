735d7c170ce7b8956a6a9d43225e210d1b0385fd
rjk-info <therjkinfo@gmail.com>
2025-07-11 23:56:53 +0530

    add

---
+++ MyCode/BitManipulation/_1AndOR.java
@@ -0,0 +1,14 @@
+public class _1AndOR{
+    public static void main(String[] args) {
+        System.out.println(5 & 6); // Output: 4
+
+        System.out.println(5 | 6); // Output: 7
+
+        System.out.println(5 ^ 6); // Output: 3
+
+        System.out.println(~5); // Output: -6
+
+
+
+    }
+}
