74f25758337bbb196b1fe6775c4730e65f72e5f1
rjk-info <therjkinfo@gmail.com>
2025-07-17 10:40:38 +0530

    add

---
+++ MyCode/Recursion/_2PrintNInc.java
@@ -0,0 +1,18 @@
+public class _2PrintNInc {
+
+    public static void printInc(int n) {
+
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+    printInc(n - 1);
+    System.out.print(n+" ");
+}
+    public static void main(String[] args) {
+        int n = 5 ;
+        printInc(n);
+    }
+}
+
+
