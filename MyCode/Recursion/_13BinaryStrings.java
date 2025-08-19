92113923ca30bcd9ee68484e005997c601ded803
rjk-info <therjkinfo@gmail.com>
2025-08-19 16:02:18 +0530

    code more

--- MyCode/Recursion/_13BinaryStrings.java
+++ MyCode/Recursion/_13BinaryStrings.java
@@ -1,12 +1,20 @@
 public class _13BinaryStrings {
 
-    public static void printBinStrings(int n, int lastPlace, StringBuilder str) {
-        // kaam 
-        if(lastPlace == 0) {
-            // sit 0 on 
+    public static void printBinStrings(int n, int lastPlace, String str) {
+        // base case 
+
+        if(n==0) {
+            System.out.println(str);
+            return;
         }
-    }
+        // kaam 
+
+        printBinStrings(n-1, 0, str+"0");
+        if (lastPlace == 0) {
+            printBinStrings(n-1, 1, str+"1");
+        }
+        }
     public static void main(String[] args) {
-         
+         printBinStrings(3, 0, new String(""));
     }
 }
